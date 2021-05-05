a8f9bcbac4162b253d36b6d48bcc025e06950238
Sonia133 <sorina.andrei2408@gmail.com>
2021-05-05 15:04:01 +0300

    update twilio credentials

--- back/functions/util/constants/configTwilio.js
+++ back/functions/util/constants/configTwilio.js
@@ -3,6 +3,6 @@ module.exports = {
       accountSid: 'AC04f65d1c07fb8239c88b7c64ea8086ea',
       apiKey: 'SKd8c939070f0008517f1cc2a78cb25e91',
       apiSecret: 'ExDXutmnpu5hF8UKqmXO36RGQNIjTopa',
-      authToken: 'bb13d7f84d5c67adc6e1bd0721f2be4e'
+      authToken: 'f1d29076e26dce537a00e7b0aef597ab'
     }
   };
